1cbcddbfc6106a6c5f7ea255ac3e606e6ad3560f
Ivan Zotov <c1825846@gmail.com>
2021-03-26 08:57:49 +0700

    added some cart functions

--- js/main.js
+++ js/main.js
@@ -18,22 +18,6 @@ const loghGoodsList = document.querySelector('.long-goods-list')
 const cartTableGoods = document.querySelector('.cart-table__goods')
 const cardTableTotal = document.querySelector('.card-table__total')
 
-const cart = {
-    cartGoods: [
-        { id: '099', name: 'Часы Dior', price: 999, count: 2 },
-        { id: '090', name: 'Кеды Nike', price: 99, count: 3 },
-    ],
-    renderCart() {
-        cartTableGoods.textContent = ''
-    },
-    deleteGood(id) {},
-    minusGood(id) {},
-    plusGood(id) {},
-    addCartGoods(id) {},
-}
-
-cart.renderCart()
-
 const getGoods = async () => {
     const result = await fetch('db/db.json')
     if (!result.ok) {
@@ -42,7 +26,98 @@ const getGoods = async () => {
     return await result.json()
 }
 
+const cart = {
+    cartGoods: [
+        { id: '099', name: 'Часы Dior', price: 999, count: 2 },
+        { id: '090', name: 'Кеды Nike', price: 99, count: 3 },
+    ],
+    renderCart() {
+        cartTableGoods.textContent = ''
+        this.cartGoods.forEach(({ id, name, price, count }) => {
+            const trGood = document.createElement('tr')
+            trGood.className = 'cart-item'
+            trGood.dataset.id = id
+            trGood.innerHTML = `
+            <td>${name}</td>
+            <td>${price}$</td>
+            <td><button class="cart-btn-minus">-</button></td>
+            <td>${count}</td>
+            <td><button class="cart-btn-plus">+</button></td>
+            <td>${price * count}$</td>
+            <td><button class="cart-btn-delete">x</button></td>
+            `
+            cartTableGoods.append(trGood)
+        })
+        const totalPrice = this.cartGoods.reduce((sum, item) => {
+            return sum + item.price * item.count
+        }, 0)
+        cardTableTotal.textContent = totalPrice
+    },
+    deleteGood(id) {
+        this.cartGoods = this.cartGoods.filter(item => id !== item.id)
+        this.renderCart()
+    },
+    minusGood(id) {
+        for (const item of this.cartGoods) {
+            if (item.id === id) {
+                if (item.count <= 1) {
+                    this.deleteGood(id)
+                } else {
+                    item.count--
+                }
+                break
+            }
+        }
+        this.renderCart()
+    },
+    plusGood(id) {
+        for (const item of this.cartGoods) {
+            if (item.id === id) {
+                item.count++
+                break
+            }
+        }
+        this.renderCart()
+    },
+    addCartGoods(id) {
+        const goodItem = this.cartGoods.find(item => item.id === id)
+        if (goodItem) {
+            this.plusGood(id)
+        } else {
+            getGoods()
+                .then(data => data.find(item => item.id === id))
+                .then(({ id, name, price }) => {
+                    this.cartGoods.push({
+                        id,
+                        name,
+                        price,
+                        count: 1,
+                    })
+                })
+        }
+    },
+}
+
+cart.addCartGoods('001')
+
+cartTableGoods.addEventListener('click', e => {
+    const target = e.target
+    if (target.tagName === 'BUTTON') {
+        const id = target.closest('.cart-item').dataset.id
+        if (target.classList.contains('cart-btn-delete')) {
+            cart.deleteGood(id)
+        }
+        if (target.classList.contains('cart-btn-minus')) {
+            cart.minusGood(id)
+        }
+        if (target.classList.contains('cart-btn-plus')) {
+            cart.plusGood(id)
+        }
+    }
+})
+
 openModal = () => {
+    cart.renderCart()
     modalCart.classList.add('show')
 }
 
